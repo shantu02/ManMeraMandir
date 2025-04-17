60f7576914e38e12f810f8f58e4e3265fa8098d1
Shantanu Ingale <shantanuingale2002@gmail.com>
2025-04-17 09:29:15 +0530

    updating the maxage for supabase session to 12 hrs in middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -19,8 +19,11 @@ export async function updateSession(request: NextRequest) {
           supabaseResponse = NextResponse.next({
             request,
           })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+          cookiesToSet.forEach(({ name, value, options }) =>{
+            options.maxAge = 43200; // set to 12 hrs
+            supabaseResponse.cookies.set(name, value, options);
+          }
+            
           )
         },
       },
