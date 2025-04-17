70279a5e10a81a4130b4311a1ea9e2cf1a35cd99
Shantanu Ingale <shantanuingale2002@gmail.com>
2025-04-17 10:08:38 +0530

    udpating error handling with try catch block

--- app/api/login/action.ts
+++ app/api/login/action.ts
@@ -10,20 +10,25 @@ interface Credentials{
 }
 
 export async function login(credentials:Credentials) {
-    const supabase = await createClient();
+    try{
+        const supabase = await createClient();
 
-    // to get user use {error, data} then data.user
-    const { error } = await supabase.auth.signInWithPassword(credentials);
-    
-    if(error){
-        console.log("error is ", error);
-        return (error instanceof AuthRetryableFetchError || error.code == "ENOTFOUND") 
-        ? {error: "Server connnection error. Please check internet connection!!!"}
-        : (error.code == "invalid_credentials")
-          ? {error: "Invalid Credentials!!!"}
-          : {error: "Unexpected Error!!!"} 
+        // to get user use {error, data} then data.user
+        const { error } = await supabase.auth.signInWithPassword(credentials);
+        
+        if(error){
+            console.log("error is ", error);
+            return (error instanceof AuthRetryableFetchError || error.code == "ENOTFOUND") 
+                    ? {error: "Server connnection error. Please check internet connection!!!"}
+                    : (error.code == "invalid_credentials")
+                        ? {error: "Invalid Credentials!!!"}
+                        : {error: "Unexpected Error!!!"} 
+        }
+        
+        return {success:true};
     }
-    
-    return {success:true};
+    catch(e){
+        return {error: "Server connnection error. Please check internet connection!!!"}
+    }
 }
 
